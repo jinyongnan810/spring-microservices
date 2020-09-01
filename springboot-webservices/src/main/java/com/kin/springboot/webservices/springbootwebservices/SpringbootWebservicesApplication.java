bca3df0d80a989f2f883da48c0bef75bacf9b194
Yuunan Kin <yuunan.kin@gmail.com>
2020-09-01 17:26:17 +0900

    server i18n

--- springboot-webservices/src/main/java/com/kin/springboot/webservices/springbootwebservices/SpringbootWebservicesApplication.java
+++ springboot-webservices/src/main/java/com/kin/springboot/webservices/springbootwebservices/SpringbootWebservicesApplication.java
@@ -2,6 +2,11 @@ package com.kin.springboot.webservices.springbootwebservices;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.i18n.AcceptHeaderLocaleResolver;
+
+import java.util.Locale;
 
 @SpringBootApplication
 public class SpringbootWebservicesApplication {
@@ -10,4 +15,11 @@ public class SpringbootWebservicesApplication {
 		SpringApplication.run(SpringbootWebservicesApplication.class, args);
 	}
 
+
+	@Bean
+	public LocaleResolver localeResolver() {
+		AcceptHeaderLocaleResolver localeResolver = new AcceptHeaderLocaleResolver();
+		localeResolver.setDefaultLocale(Locale.US);
+		return localeResolver;
+	}
 }
--- springboot-webservices/src/main/java/com/kin/springboot/webservices/springbootwebservices/controller/UserController.java
+++ springboot-webservices/src/main/java/com/kin/springboot/webservices/springbootwebservices/controller/UserController.java
@@ -4,6 +4,8 @@ import com.kin.springboot.webservices.springbootwebservices.entity.User;
 import com.kin.springboot.webservices.springbootwebservices.exception.UserNotFoundException;
 import com.kin.springboot.webservices.springbootwebservices.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
@@ -14,6 +16,8 @@ import java.util.Optional;
 public class UserController {
     @Autowired
     private UserService service;
+    @Autowired
+    private MessageSource messageSource;
 
     @GetMapping("/users")
     public List<User> getUsers(){
@@ -44,5 +48,9 @@ public class UserController {
             return;
         }
         throw new UserNotFoundException("id:"+id);
+    }
+    @GetMapping("/hello")
+    public String hello(){
+        return messageSource.getMessage("hello",null, LocaleContextHolder.getLocale());
     }
 }
