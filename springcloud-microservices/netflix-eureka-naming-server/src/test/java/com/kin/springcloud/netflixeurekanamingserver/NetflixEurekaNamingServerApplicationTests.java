a3d69cbabd791c872c53a1ed515487512e84f550
Yuunan Kin <yuunan.kin@gmail.com>
2020-09-09 17:29:00 +0900

    feign, ribbon, eureka

---
+++ springcloud-microservices/netflix-eureka-naming-server/src/test/java/com/kin/springcloud/netflixeurekanamingserver/NetflixEurekaNamingServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kin.springcloud.netflixeurekanamingserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NetflixEurekaNamingServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
